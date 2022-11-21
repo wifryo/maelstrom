323cd3df1200e81eabdbceeab61485cb3c705ae4
William <wifryo@gmail.com>
2022-11-21 11:19:12 +0100

    Prepare deployment

--- next.config.js
+++ next.config.js
@@ -2,6 +2,28 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  compiler: {
+    emotion: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  // Fix issue with `node:` scheme / prefix
+  webpack: (config, { webpack }) => {
+    config.plugins.push(
+      // Remove node: from import specifiers, because
+      // Next.js does not yet support node: scheme
+      // https://github.com/vercel/next.js/issues/28774
+      new webpack.NormalModuleReplacementPlugin(/^node:/, (resource) => {
+        resource.request = resource.request.replace(/^node:/, '');
+      }),
+    );
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
