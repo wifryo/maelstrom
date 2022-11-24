0a39214a2a8aae7b3e9132c40814a29cbda0027f
William <wifryo@gmail.com>
2022-11-24 08:38:58 +0100

    Implement header responsiveness

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,14 @@
 import { css } from '@emotion/react';
+import MenuIcon from '@mui/icons-material/Menu';
 import AppBar from '@mui/material/AppBar';
 import Grid from '@mui/material/Grid';
+import IconButton from '@mui/material/IconButton';
 import Link from '@mui/material/Link';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import React from 'react';
 import { User } from '../database/users';
 
 type Props = {
@@ -22,6 +27,16 @@ function Anchor({ children, ...restProps }: any) {
 }
 
 export default function Header(props: Props) {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <AppBar
       position="fixed"
@@ -38,7 +53,12 @@ export default function Header(props: Props) {
     >
       <Toolbar disableGutters={true}>
         <Grid container alignItems="center" height="3rem">
-          <Grid container item xs={4}>
+          <Grid
+            container
+            item
+            xs={4}
+            sx={{ display: { xs: 'none', sm: 'flex' } }}
+          >
             <Link href="/generators" underline="none" mr="1rem">
               <Typography variant="body2">generators</Typography>
             </Link>
@@ -54,7 +74,13 @@ export default function Header(props: Props) {
               </Typography>
             </Link>
           </Grid>
-          <Grid container item xs={4} justifyContent="flex-end">
+          <Grid
+            container
+            item
+            xs={4}
+            justifyContent="flex-end"
+            sx={{ display: { xs: 'none', sm: 'flex' } }}
+          >
             <Link href="/private-profile" underline="none" mr="1rem">
               <Typography variant="body2">
                 {props.user && props.user.username}
@@ -75,6 +101,62 @@ export default function Header(props: Props) {
                 </Link>
               </>
             )}
+          </Grid>
+          <Grid
+            container
+            item
+            xs={4}
+            justifyContent="flex-end"
+            sx={{ display: { xs: 'flex', sm: 'none' } }}
+          >
+            <div>
+              <IconButton
+                size="large"
+                aria-label="login and registration"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="#000"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                sx={{ mt: '2.5rem' }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <div>
+                  {props.user ? (
+                    <>
+                      <Link href="/generators" underline="none">
+                        <MenuItem onClick={handleClose}>generators</MenuItem>
+                      </Link>
+                      <Link href="/about" underline="none">
+                        <MenuItem onClick={handleClose}>about</MenuItem>
+                      </Link>
+                      <Link href="/private-profile" underline="none">
+                        <MenuItem onClick={handleClose}>profile</MenuItem>
+                      </Link>
+                      <Anchor href="/logout" css={anchorStyles}>
+                        <MenuItem onClick={handleClose}>logout</MenuItem>
+                      </Anchor>
+                    </>
+                  ) : (
+                    <Link href="/login" underline="none">
+                      <MenuItem onClick={handleClose}>login</MenuItem>
+                    </Link>
+                  )}
+                </div>
+              </Menu>
+            </div>
           </Grid>
         </Grid>
       </Toolbar>
